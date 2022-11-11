c91a0885372c9529893e52c321934dd65930fe4c
Milen961 <theheller178@gmail.com>
2022-11-11 13:54:52 +0200

    Create core components and connect enviroment to URL

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import  { CoreModule  } from './core/core.module'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ThemeListComponent } from './theme-list/theme-list.component';
+import { RecentPostsComponent } from './recent-posts/recent-posts.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ThemeListComponent,
+    RecentPostsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    CoreModule,
+    HttpClientModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
