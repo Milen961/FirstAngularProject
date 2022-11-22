ef5312b31a9a21bbed7392ecb3da0e406c4e5373
Milen961 <theheller178@gmail.com>
2022-11-22 14:40:07 +0200

    Add a profile component and path for it

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,6 +1,7 @@
 import { RouterModule, Routes } from "@angular/router";
 import { LoginComponent } from "./login/login.component";
 import { LogoutComponent } from "./logout/logout.component";
+import { ProfileComponent } from "./profile/profile.component";
 import { RegisterComponent } from "./register/register.component";
 
 const routes: Routes = [
@@ -18,6 +19,12 @@ const routes: Routes = [
     {
         path: 'auth/logout',
         component: LogoutComponent
+
+    },
+
+    {
+        path: 'auth/profile',
+        component: ProfileComponent
 
     }
 
