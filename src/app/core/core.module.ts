86877085cadc70ce44ecf6b64603777e52879a40
Milen961 <theheller178@gmail.com>
2022-11-25 13:48:03 +0200

    Update core module

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,27 +2,36 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { RouterModule } from '@angular/router';
-
-
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { HomeComponent } from './home/home.component';
+import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
   declarations: [
+    HomeComponent,
     HeaderComponent,
     FooterComponent,
     PageNotFoundComponent
   ],
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    SharedModule,
+    // MyModule
+  ],
+  providers: [
+    // {
+    //   provide: 'Test',
+    //   useValue: 'Best'
+    // }
   ],
   exports: [
+    HomeComponent,
     HeaderComponent,
     FooterComponent,
-    PageNotFoundComponent,
-    
+    PageNotFoundComponent
   ]
 })
 export class CoreModule { }
